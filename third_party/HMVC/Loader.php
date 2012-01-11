df36fb8fbee268b58ec6f87eb3e0f2192abf33ea
Jens Segers <segers.jens@gmail.com>
2012-01-12 00:44:22 +0100

    Avoid double module loading + tweaks

--- third_party/HMVC/Loader.php
+++ third_party/HMVC/Loader.php
@@ -30,6 +30,14 @@ if (!defined("BASEPATH"))
 class HMVC_Loader extends CI_Loader {
     
     /**
+     * List of loaded models
+     *
+     * @var array
+     * @access protected
+     */
+    protected $_ci_modules = array();
+    
+    /**
      * Constructor
      *
      * Add the current module to all paths permanently
@@ -37,16 +45,10 @@ class HMVC_Loader extends CI_Loader {
     public function __construct() {
         parent::__construct();
         
+        // Get current module from the router
         $router = & $this->_ci_get_component('router');
         if ($router->module) {
-            $path = APPPATH . 'modules/' . $router->module . '/';
-            
-            array_unshift($this->_ci_library_paths, $path);
-            array_unshift($this->_ci_model_paths, $path);
-            $this->_ci_view_paths[$path . 'views/'] = 1;
-            
-            $config = & $this->_ci_get_component('config');
-            array_unshift($config->_config_paths, $path);
+            $this->add_module($router->module);
         }
     }
     
@@ -70,15 +72,22 @@ class HMVC_Loader extends CI_Loader {
         }
         
         // Detect module
-        if (list($class, $module) = $this->detect_module($library)) {
-            // Add module as a package
-            array_unshift($this->_ci_library_paths, APPPATH . 'modules/' . $module);
+        if (list($module, $class) = $this->detect_module($library)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::library($class, $params, $object_name);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            return parent::library($class, $params, $object_name);
+            $void = parent::library($class, $params, $object_name);
             
-            // Remove package again
-            array_shift($this->_ci_library_paths);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             return parent::library($library, $params, $object_name);
         }
@@ -103,15 +112,22 @@ class HMVC_Loader extends CI_Loader {
         }
         
         // Detect module
-        if (list($class, $module) = $this->detect_module($model)) {
-            // Add module as a package
-            array_unshift($this->_ci_model_paths, APPPATH . 'modules/' . $module);
+        if (list($module, $class) = $this->detect_module($model)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::model($class, $name, $db_conn);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            return parent::model($class, $name, $db_conn);
+            $void = parent::model($class, $name, $db_conn);
             
-            // Remove package again
-            array_shift($this->_ci_model_paths);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             return parent::model($model, $name, $db_conn);
         }
@@ -135,15 +151,22 @@ class HMVC_Loader extends CI_Loader {
      */
     public function view($view, $vars = array(), $return = FALSE) {
         // Detect module
-        if (list($class, $module) = $this->detect_module($view)) {
-            // Add module as a package
-            $this->_ci_view_paths[APPPATH . 'modules/' . $module . '/views/'] = 1;
+        if (list($module, $class) = $this->detect_module($view)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::view($class, $vars, $return);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            return parent::view($class, $vars, $return);
+            $void = parent::view($class, $vars, $return);
             
-            // Remove package again
-            unset($this->_ci_view_paths[APPPATH . 'modules/' . $module . '/views/']);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             return parent::view($view, $vars, $return);
         }
@@ -159,16 +182,22 @@ class HMVC_Loader extends CI_Loader {
      */
     public function config($file = '', $use_sections = FALSE, $fail_gracefully = FALSE) {
         // Detect module
-        if (list($class, $module) = $this->detect_module($file)) {
-            // Add module as a package
-            $config = & $this->_ci_get_component('config');
-            array_unshift($config->_config_paths, APPPATH . 'modules/' . $module);
+        if (list($module, $class) = $this->detect_module($file)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::config($class, $use_sections, $fail_gracefully);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            parent::config($class, $use_sections, $fail_gracefully);
+            $void = parent::config($class, $use_sections, $fail_gracefully);
             
-            // Remove package again
-            array_shift($config->_config_paths);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             parent::config($file, $use_sections, $fail_gracefully);
         }
@@ -191,15 +220,22 @@ class HMVC_Loader extends CI_Loader {
         }
         
         // Detect module
-        if (list($class, $module) = $this->detect_module($helper)) {
-            // Add module as a package
-            array_unshift($this->_ci_helper_paths, APPPATH . 'modules/' . $module);
+        if (list($module, $class) = $this->detect_module($helper)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::helper($class);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            return parent::helper($class);
+            $void = parent::helper($class);
             
-            // Remove package again
-            array_shift($this->_ci_helper_paths);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             return parent::helper($helper);
         }
@@ -221,19 +257,68 @@ class HMVC_Loader extends CI_Loader {
         }
         
         // Detect module
-        if (list($class, $module) = $this->detect_module($file)) {
-            // Add module as a package
-            array_unshift($this->_ci_library_paths, APPPATH . 'modules/' . $module);
-            array_unshift($this->_ci_model_paths, APPPATH . 'modules/' . $module);
+        if (list($module, $class) = $this->detect_module($file)) {
+            // Module already loaded
+            if (in_array($module, $this->_ci_modules)) {
+                return parent::language($class, $lang);
+            }
+            
+            // Add module
+            $this->add_module($module);
             
             // Let parent do the heavy work
-            return parent::language($class, $lang);
+            $void = parent::language($class, $lang);
             
-            // Remove package again
-            array_shift($this->_ci_library_paths);
-            array_shift($this->_ci_model_paths);
+            // Remove module
+            $this->remove_module();
+            
+            return $void;
         } else {
             return parent::language($file, $lang);
+        }
+    }
+    
+    /**
+     * Add Module
+     *
+     * Allow resources to be loaded from this module path
+     *
+     * @param	string
+     * @param 	boolean
+     * @return	void
+     */
+    public function add_module($module, $view_cascade = TRUE) {
+        // Mark module as loaded
+        array_unshift($this->_ci_modules, $module);
+        
+        // Add package path
+        $path = APPPATH . 'modules/' . rtrim($module, '/') . '/';
+        return parent::add_package_path($path, $view_cascade);
+    }
+    
+    /**
+     * Remove Module
+     *
+     * Remove a module from the allowed module paths
+     *
+     * @param	type
+     * @param 	bool
+     * @return	type
+     */
+    public function remove_module($module = '', $remove_config = TRUE) {
+        if ($module == '') {
+            // Mark module as not loaded
+            array_shift($this->_ci_modules);
+
+            // Remove package path
+            return parent::remove_package_path('', $remove_config);
+        } else if (($key = array_search($module, $this->_ci_modules)) !== FALSE) {
+            // Mark module as not loaded
+            unset($this->_ci_modules[$key]);
+            
+            // Remove package path
+            $path = APPPATH . 'modules/' . rtrim($module, '/') . '/';
+            return parent::remove_package_path($path, $remove_config);
         }
     }
     
@@ -246,13 +331,14 @@ class HMVC_Loader extends CI_Loader {
     private function detect_module($class) {
         $class = str_replace('.php', '', trim($class, '/'));
         if (($first_slash = strpos($class, '/')) !== FALSE) {
-            $module = substr($class, 0, $first_slash + 1);
+            $module = substr($class, 0, $first_slash);
             $class = substr($class, $first_slash + 1);
             
-            if (is_dir(APPPATH . 'modules/' . $module))
-                return array($class, $module);
-            else
+            if (is_dir(APPPATH . 'modules/' . $module)) {
+                return array($module, $class);
+            } else {
                 return FALSE;
+            }
         }
         return FALSE;
     }
