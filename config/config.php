06a0a3bf5bd76d05afc8f7c9c6ee0f0bfeedd55e
Jens Segers <segers.jens@gmail.com>
2012-02-03 15:22:03 +0100

    Sparks file

---
+++ config/config.php
@@ -0,0 +1,11 @@
+/*
+|--------------------------------------------------------------------------
+| Modules locations
+|--------------------------------------------------------------------------
+|
+| These are the folders where your modules are located. You may define an
+| absolute path to the location or a relative path starting from the root
+| directory.
+|
+*/
+$config['modules_locations'] = array(APPPATH . "modules/");
