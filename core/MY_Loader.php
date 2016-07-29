25675e34c83ed40649770a1a9b300a2ff266a740
w3prodigy <w3prodigy@gmail.com>
2016-07-29 17:32:33 -0400

    moved application files into application directory to allow for hmvc module code to be used as an upstream

--- core/MY_Loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-(defined('BASEPATH')) or exit('No direct script access allowed');
-
-/* load the HMVC_Loader class */
-require APPPATH . 'third_party/HMVC/Loader.php';
-
-class MY_Loader extends HMVC_Loader {
-}
